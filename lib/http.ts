57d8008a58e7285c3dbaffd5f2d7404641521c9f
codecrusher <len.knoche@gmail.com>
2026-02-26 21:57:16 +0100

    update: even more updates to due pending problem and admin approvevment error

--- lib/http.ts
+++ lib/http.ts
@@ -1,5 +1,5 @@
 const UUID_REGEX =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export function getClientIp(request: Request): string {
   const forwardedFor = request.headers.get('x-forwarded-for');
